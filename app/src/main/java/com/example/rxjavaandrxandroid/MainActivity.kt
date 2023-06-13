199df0a3838012928c9647cdccf318e8ad24f37d
Houwen Lie <houwenlie98@gmail.com>
2023-06-13 19:18:04 +0700

    update copy right

--- app/src/main/java/com/example/rxjavaandrxandroid/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/MainActivity.kt
@@ -5,7 +5,7 @@ import com.example.rxjavaandrxandroid.presentation.ViewBindingActivity
 
 /**
  * @author Houwen Lie (houwenlie98@gmail.com)
- * @version asd, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
+ * @version MainActivity, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
  */
 
 class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
--- app/src/main/java/com/example/rxjavaandrxandroid/presentation/ViewBindingActivity.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/presentation/ViewBindingActivity.kt
@@ -6,7 +6,7 @@ import io.reactivex.disposables.CompositeDisposable
 
 /**
  * @author Houwen Lie (houwenlie98@gmail.com)
- * @version asd, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
+ * @version ViewBindingActivity, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
  */
 abstract class ViewBindingActivity<VB: ViewBinding> : ComponentActivity() {
 
