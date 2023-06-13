c25c000b68ac8b7b734cfba512c770101c775e51
Houwen Lie <houwenlie98@gmail.com>
2023-06-13 19:09:42 +0700

    Add copyrights

--- app/src/main/java/com/example/rxjavaandrxandroid/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.example.rxjavaandrxandroid.databinding.ActivityMainBinding
 
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version asd, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
+ */
 
 class MainActivity : ComponentActivity() {
 
--- app/src/main/java/com/example/rxjavaandrxandroid/presentation/ViewBindingActivity.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/presentation/ViewBindingActivity.kt
@@ -1,8 +1,13 @@
 package com.example.rxjavaandrxandroid.presentation
 
+import androidx.activity.ComponentActivity
 import androidx.viewbinding.ViewBinding
 
-abstract class ViewBindingActivity<VB: ViewBinding> : AppComp() {
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version asd, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
+ */
+abstract class ViewBindingActivity<VB: ViewBinding> : ComponentActivity() {
 
     abstract fun inflateViewBinding() : VB
 
