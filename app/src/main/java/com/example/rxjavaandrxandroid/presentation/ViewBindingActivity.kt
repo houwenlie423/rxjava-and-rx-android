31cdaaf399a2f61ac7438e6210d8b5d012b528ae
Houwen Lie <houwenlie98@gmail.com>
2023-06-13 19:27:09 +0700

    Add open function init

--- app/src/main/java/com/example/rxjavaandrxandroid/presentation/ViewBindingActivity.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/presentation/ViewBindingActivity.kt
@@ -1,5 +1,7 @@
 package com.example.rxjavaandrxandroid.presentation
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.viewbinding.ViewBinding
 import io.reactivex.disposables.CompositeDisposable
@@ -8,13 +10,22 @@ import io.reactivex.disposables.CompositeDisposable
  * @author Houwen Lie (houwenlie98@gmail.com)
  * @version ViewBindingActivity, v 0.1 Tue 6/13/2023 7:05 PM by Houwen Lie
  */
-abstract class ViewBindingActivity<VB: ViewBinding> : ComponentActivity() {
+abstract class ViewBindingActivity<VB : ViewBinding> : ComponentActivity() {
 
-    abstract fun inflateViewBinding() : VB
+    abstract fun inflateViewBinding(): VB
 
     protected val binding: VB by lazy(LazyThreadSafetyMode.NONE) { inflateViewBinding() }
 
     protected val disposeBag by lazy(LazyThreadSafetyMode.SYNCHRONIZED) { CompositeDisposable() }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        init(savedInstanceState, persistentState)
+    }
+
+    open fun init(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        // no implementation on base
+    }
 
     override fun onDestroy() {
         super.onDestroy()
