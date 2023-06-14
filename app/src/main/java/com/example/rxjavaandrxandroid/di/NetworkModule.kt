65c53553e17d4b58242acba320749ab1fbfe25ed
Houwen Lie <houwenlie98@gmail.com>
2023-06-14 16:25:33 +0700

    Create NetworkModule for DI

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/di/NetworkModule.kt
@@ -0,0 +1,16 @@
+package com.example.rxjavaandrxandroid.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version NetworkModule, v 0.1 Wed 6/14/2023 4:25 PM by Houwen Lie
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+}
