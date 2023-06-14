565e5b570de492d4c7e4aaa4406522ac8d9e42c3
Houwen Lie <houwenlie98@gmail.com>
2023-06-14 16:21:53 +0700

    Create new RxViewModel base class

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/presentation/base/RxViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.rxjavaandrxandroid.presentation.base
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version RxViewModel, v 0.1 Wed 6/14/2023 4:21 PM by Houwen Lie
+ */
+class RxViewModel {
+}
