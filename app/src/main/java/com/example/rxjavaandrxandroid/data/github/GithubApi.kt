4486e40e5739f1d61d6ad7cb09e5f763af8cad37
Houwen Lie <houwenlie98@gmail.com>
2023-06-14 16:41:34 +0700

    Create interfaces for NasaApi and GithubApi

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/data/github/GithubApi.kt
@@ -0,0 +1,11 @@
+package com.example.rxjavaandrxandroid.data.github
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version GithubApi, v 0.1 Wed 6/14/2023 4:40 PM by Houwen Lie
+ */
+interface GithubApi {
+
+
+}
