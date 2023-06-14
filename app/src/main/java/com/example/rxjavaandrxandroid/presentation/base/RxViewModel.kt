a0c8b6e6f52648dbec9911b130cd770848cfd283
Houwen Lie <houwenlie98@gmail.com>
2023-06-14 16:24:20 +0700

    Create abstraction for ViewModel for Rx cases

--- app/src/main/java/com/example/rxjavaandrxandroid/presentation/base/RxViewModel.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/presentation/base/RxViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.rxjavaandrxandroid.presentation.base
 
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
 
 /**
  * @author Houwen Lie (houwenlie98@gmail.com)
  * @version RxViewModel, v 0.1 Wed 6/14/2023 4:21 PM by Houwen Lie
  */
-class RxViewModel {
+abstract class RxViewModel() : ViewModel() {
+
+    protected val disposeBag by lazy { CompositeDisposable() }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposeBag.dispose()
+    }
 }
