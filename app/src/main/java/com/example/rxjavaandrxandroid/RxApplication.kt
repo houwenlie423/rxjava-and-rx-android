ecee6860782f48ab0f8aca76ff45e6bd422691de
Houwen Lie <houwenlie98@gmail.com>
2023-06-15 15:27:25 +0700

    Create RxApplication as Application Entry Point for dagger hilt

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/RxApplication.kt
@@ -0,0 +1,12 @@
+package com.example.rxjavaandrxandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version RxApplication, v 0.1 Thu 6/15/2023 3:26 PM by Houwen Lie
+ */
+@HiltAndroidApp
+class RxApplication : Application()
