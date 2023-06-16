3899b137c3123d193f10fd0c88600eb9d3144f65
Houwen Lie <houwenlie98@gmail.com>
2023-06-16 17:41:32 +0700

    Add Url Util and Github base url constant

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/shared/UrlUtil.kt
@@ -0,0 +1,11 @@
+package com.example.rxjavaandrxandroid.shared
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version UrlUtil, v 0.1 Fri 6/16/2023 5:39 PM by Houwen Lie
+ */
+object UrlUtil {
+
+    const val GITHUB_API_BASE_URL = "https://api.github.com/octocat"
+}
