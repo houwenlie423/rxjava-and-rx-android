9f01c6df21354f1b8bf715d0ee6f4a01af28ad86
Houwen Lie <houwenlie98@gmail.com>
2023-06-22 09:24:39 +0700

    Create fake user model

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/models/FakeUser.kt
@@ -0,0 +1,11 @@
+package com.example.rxjavaandrxandroid.models
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version FakeUser, v 0.1 Thu 6/22/2023 9:24 AM by Houwen Lie
+ */
+data class FakeUser(
+    val id: Int,
+    val name: String
+)
