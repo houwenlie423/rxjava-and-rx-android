a1a048698b875371fbd5cb31bab2e6b61056a2c6
Houwen Lie <houwenlie98@gmail.com>
2023-06-23 22:32:00 +0700

    Tidy up rx ext

--- app/src/main/java/com/example/rxjavaandrxandroid/utils/RxExt.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/utils/RxExt.kt
@@ -1,8 +1,11 @@
 package com.example.rxjavaandrxandroid.utils
 
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableCompletableObserver
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
@@ -13,13 +16,18 @@ import io.reactivex.schedulers.Schedulers
  * @version RxExt, v 0.1 Fri 6/16/2023 7:13 PM by Houwen Lie
  */
 
-fun <T> Observable<T>.applySchedulers(): Observable<T> = this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+fun <T> Observable<T>.applySchedulers(
+    threadExecutor: Scheduler = Schedulers.io(),
+    postThreadExecutor: Scheduler = AndroidSchedulers.mainThread()
+): Observable<T> = this.subscribeOn(threadExecutor).observeOn(postThreadExecutor)
 
 inline fun <reified T> Observable<T>.execute(
     crossinline onSuccess: (T) -> Unit = {},
     crossinline onError: (Throwable) -> Unit = {},
+    threadExecutor: Scheduler = Schedulers.io(),
+    postThreadExecutor: Scheduler = AndroidSchedulers.mainThread()
 ) {
-    this.applySchedulers()
+    this.applySchedulers(threadExecutor, postThreadExecutor)
         .subscribe(object: DisposableObserver<T>() {
             override fun onNext(t: T) {
                 LogUtil.log("[${T::class.simpleName}] Success = $t")
@@ -62,11 +70,37 @@ fun <T> Observable<T>.collectAsSingle(): Single<T> {
     }
 }
 
-fun <T> Single<T>.applySchedulers() = this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+fun <T> Single<T>.applySchedulers(
+    threadExecutor: Scheduler = Schedulers.io(),
+    postThreadExecutor: Scheduler = AndroidSchedulers.mainThread()
+) = this.subscribeOn(threadExecutor).observeOn(postThreadExecutor)
 
 inline fun <reified T> Single<T>.execute(
     crossinline onSuccess: (T) -> Unit = {},
     crossinline onError: (Throwable) -> Unit = {},
+    threadExecutor: Scheduler = Schedulers.io(),
+    postThreadExecutor: Scheduler = AndroidSchedulers.mainThread()
 ) {
-    this.toObservable().execute(onSuccess, onError)
+    this.toObservable().execute(onSuccess, onError, threadExecutor, postThreadExecutor)
+}
+
+fun Completable.applySchedulers(
+    threadExecutor: Scheduler = Schedulers.io(),
+    postThreadExecutor: Scheduler = AndroidSchedulers.mainThread()
+) = this.subscribeOn(threadExecutor).observeOn(postThreadExecutor)
+
+fun executeInBackground(action: () -> Unit) {
+    Completable.fromAction(action)
+        .applySchedulers()
+        .subscribe(object : DisposableCompletableObserver() {
+            override fun onComplete() {
+                LogUtil.log("[$action] Complete")
+                dispose()
+            }
+
+            override fun onError(e: Throwable) {
+                LogUtil.log("[$action] Complete")
+                dispose()
+            }
+        })
 }
