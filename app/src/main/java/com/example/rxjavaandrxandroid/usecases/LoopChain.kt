08d8c3249b15d40733e5e2a6e94ef714f7dcde84
Houwen Lie <houwenlie98@gmail.com>
2023-06-23 21:56:15 +0700

    Create usecase loop chain

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/usecases/LoopChain.kt
@@ -0,0 +1,14 @@
+package com.example.rxjavaandrxandroid.usecases
+
+import com.example.rxjavaandrxandroid.api.FakeApi
+import javax.inject.Inject
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version LoopChain, v 0.1 Fri 6/23/2023 9:55 PM by Houwen Lie
+ */
+class LoopChain @Inject constructor(
+    private val fakeApi: FakeApi
+) {
+}
