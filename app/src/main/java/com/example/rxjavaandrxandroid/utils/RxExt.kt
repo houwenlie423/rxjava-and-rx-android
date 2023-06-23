7f29114c71dffe03762be5b6d1b29484c1624d4b
Houwen Lie <houwenlie98@gmail.com>
2023-06-23 22:16:56 +0700

    Create inline function execute

--- app/src/main/java/com/example/rxjavaandrxandroid/utils/RxExt.kt
+++ app/src/main/java/com/example/rxjavaandrxandroid/utils/RxExt.kt
@@ -3,6 +3,8 @@ package com.example.rxjavaandrxandroid.utils
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 
 
@@ -11,6 +13,32 @@ import io.reactivex.schedulers.Schedulers
  * @version RxExt, v 0.1 Fri 6/16/2023 7:13 PM by Houwen Lie
  */
 
-fun <T> Observable<T>.applySchedulers() = this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+fun <T> Observable<T>.applySchedulers(): Observable<T> = this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+inline fun <reified T> Observable<T>.execute(
+    crossinline onSuccess: (T) -> Unit = {},
+    crossinline onError: (Throwable) -> Unit = {},
+) {
+    this.applySchedulers()
+        .subscribe(object: DisposableObserver<T>() {
+            override fun onNext(t: T) {
+                LogUtil.log("[${T::class.simpleName}] Success = $t")
+                onSuccess(t)
+                dispose()
+            }
+
+            override fun onError(e: Throwable) {
+                LogUtil.log("[${T::class.simpleName}] Error = ${e.message}")
+                onError(e)
+                dispose()
+            }
+
+            override fun onComplete() {
+                LogUtil.log("[${T::class.simpleName}] Complete")
+                dispose()
+            }
+
+        })
+}
 
 fun <T> Single<T>.applySchedulers() = this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
