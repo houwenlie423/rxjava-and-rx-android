66cef19d27440516b92e13b9751c11749689b6a7
Houwen Lie <houwenlie98@gmail.com>
2023-06-27 22:13:37 +0700

    Add button to go to secondary activity to test launch mode

---
+++ app/src/main/java/com/example/rxjavaandrxandroid/SecondaryActivity.kt
@@ -0,0 +1,9 @@
+package com.example.rxjavaandrxandroid
+
+
+/**
+ * @author Houwen Lie (houwenlie98@gmail.com)
+ * @version SecondaryActivity, v 0.1 Tue 6/27/2023 10:11 PM by Houwen Lie
+ */
+class SecondaryActivity  {
+}
